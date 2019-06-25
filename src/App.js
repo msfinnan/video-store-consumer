1549d2934ba6d9d1505b8f54f73eab85bd0ebb42
Margaret Finnan <msfinnan@gmail.com>
2019-06-25 14:05:30 -0700

    added React Router to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import MovieList from './components/MovieList';
 import CustomerList from './components/CustomerList';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -21,23 +22,23 @@ class App extends Component {
     axios.get('http://localhost:3000/movies')
       .then((response) => {
         console.log('response.data is:', response.data);
-        this.setState({ 
-          allMovies: response.data 
+        this.setState({
+          allMovies: response.data
         });
-        console.log('allMovies is:',this.state.allMovies)
+        console.log('allMovies is:', this.state.allMovies)
       })
       .catch((error) => {
         this.setState({
           errorMessage: error.message
         })
       })
-      axios.get('http://localhost:3000/customers')
+    axios.get('http://localhost:3000/customers')
       .then((response) => {
         console.log('response.data is:', response.data);
-        this.setState({ 
-          allCustomers: response.data 
+        this.setState({
+          allCustomers: response.data
         });
-        console.log('allCustomers is:',this.state.allCustomers)
+        console.log('allCustomers is:', this.state.allCustomers)
       })
       .catch((error) => {
         this.setState({
@@ -45,14 +46,33 @@ class App extends Component {
         })
       })
   }
-  
+
+
   render() {
     return (
-      <div>
-        Video Store App
-        < MovieList allMovies={this.state.allMovies}/> 
-        < CustomerList allCustomers={this.state.allCustomers}/> 
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route
+            path="/movies"
+            render={(props) => <MovieList allMovies={this.state.allMovies} isAuthed={true} />}
+          />
+          <Route 
+            path="/customers" 
+            render={(props) => <CustomerList allCustomers={this.state.allCustomers} isAuthed={true} />}
+          />
+        </div>
+      </Router>
     );
   }
 }
