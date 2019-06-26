aea6904e8043cabbda6814b6bcf3105011eeb2f6
laneia <laneiamurray@gmail.com>
2019-06-26 14:39:38 -0700

    basic search functionality, cannot select from it

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,114 +2,87 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import Movie from './Movie';
-
 class Search extends Component {
 
   constructor(props) {
     super(props);
-
     this.cleared = {
       query: "",
-      queryResults: [],
+      queryResults: undefined,
     }
 
-    this.state = { ...this.cleared}
+    this.state = { ...this.cleared }
   }
 
-  onFormInput = (e) => {
-    e.preventDefault();
-
-    const query = this.state.query;
-    console.log(query)
-
-    this.onSubmit(query)
-
-    this.setState({ ...this.cleared });
-  }
-
-  onSubmit = (query) => {
-    const KEY = process.env.REACT_APP_MOVIEDB_KEY
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${query}`
-  
-    axios.get(URL)
-      .then((response) => {
-        const currentSearch = response.data.results.map((movie) => {
-          
-          const searchedMovie = { 
-            title: movie.title,
-            overview: movie.overview,
-            release_date: movie.release_date
-          }
-          
-          return searchedMovie
-        })
-  
-        this.setState({queryResults: currentSearch});
-        console.log(response.data);
-      })
-  
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-
-  onInputChange = (e) => {
-    const updatedState = {};
-
-    const field = e.target.name;
-    const value = e.target.value;
-
-    updatedState[field] = value;
-    this.setState(updatedState);
-    }   
-
-  onSelectMovie = (movieId) => {
-    const selectedMovie = this.state.allMovies.find(movie => movie.id === movieId)
-    console.log('movieId is', movieId)
-    this.setState({
-        selectedMovieId: selectedMovie.id,
-        selectedMovieTitle: selectedMovie.title
-    });
-    }
-  
-    render() {
-        const displayMovie = this.state.queryResults.map((movie) => {
-          const { id, title, overview, release_date } = movie;
-          return (
-          <section>
-            <Movie
-              id={id}
-              title={title}
-              overview={overview}
-              release_date={release_date}
-              onSelectMovieCallback={this.onSelectMovie}
-              displayButton="Add to Movie List"
-            />
-          </section>);
+  searchMovies = () => {
+    axios.get('http://localhost:3000/movies?query=' + this.state.query)
+      .then(response => {
+        const movies = response.data.map(movie => {
+          return movie;
         });
+        this.setState({
+          queryResults: movies,
+        });
+      })
+      .catch((error) => {
+              console.log(error);
+            })
+  };
 
+  onInput = event => {
+    const newSearch = {};
+    newSearch[event.target.name] = event.target.value;
+    this.setState(newSearch);
+  };
+
+  findMovie = event => {
+    event.preventDefault();
+    this.searchMovies();
+  };
+
+  movieSearchList = () => {
+    return this.state.queryResults.map(movie => {
+      return (
+        <Movie
+          key={movie.id}
+          {...movie}
+          // isSearch={this.state.isSearch}
+          // addToLibrary={this.props.addToLibrary}
+        />
+      );
+    });
+  };
+
+  render() {
     return (
       <div>
         <section>
-            <form
-                onSubmit = {this.onFormInput}>
-            <label>
-                Movie title:
-            <input
-              name="query"
-              type="text"
-              value = {this.state.query}
-              onChange = {this.onInputChange}
-               >
-            </input>
-            </label>
-            <input type="submit" name="submit" value="Search"/>
-            </form>
+          <form onSubmit={this.findMovie}>
+            <label htmlFor="query">Movie Search: </label>
+            <input name="query" onChange={this.onInput} />
+            <input type="submit" name="submit" value="Search" />
+          </form>
         </section>
-      <div>{displayMovie}</div>
-    </div>
+        {this.state.queryResults && this.movieSearchList()}
+      </div>
     );
   }
 }
+
+
+  //           <form
+  //               onSubmit = {this.onFormInput}>
+  //           <label>
+  //               Movie title:
+  //           <input
+  //             name="query"
+  //             type="text"
+  //             value = {this.state.query}
+  //             onChange = {this.onInputChange}
+  //              >
+  //           </input>
+  //           </label>
+  
+
 
 export default Search;
